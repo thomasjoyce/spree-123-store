3c14c201a0ee00a084a2bdfd3b9578fe4d774482
ThomasJoyce <thomasjoyce@gmail.com>
2012-01-23 15:25:01 -0800

    Installing rails new spree-123-store, Rails 3.1.3, ruby 1.9.3p0 (2011-10-30 revision 33570) [x86_64-darwin11.2.0]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree123Store::Application.config.secret_token = 'e6199d6c26b15b1d7d422b03b9f79fea0831cffa5fb38bc493a50122ba66eb29812b63cf55e9fa0ffcd8d0ce8fa2681f9519fe92311b5fc42460af04c555c8f7'
